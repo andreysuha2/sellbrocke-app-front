9660efd45723d765f64f6f7815b2da2e28a8d2b7
Andrey Suhenko <andreysuha2@gmail.com>
2020-06-18 15:58:18 +0300

    remove default components

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,4 @@
 <template>
   <section class="container">
-    <div>
-        <md-button>test</md-button>
-    </div>
   </section>
 </template>
-
-<style lang="scss">
-.container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-}
-
-.title {
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-}
-
-.subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-}
-
-.links {
-    padding-top: 15px;
-}
-</style>
-
