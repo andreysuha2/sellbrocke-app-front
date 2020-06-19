e64556afdd2b4ae051042fb5a3a358aab5c81415
Andrey Suhenko <andreysuha2@gmail.com>
2020-06-19 10:59:49 +0300

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,55 @@
 <template>
-  <div class="app-wrapper">
+  <div class="app flex flex-col">
     <app-header/>
-    <nuxt/>
+    <div class="flex app--wrapper">
+        <div class="app--menu"></div>
+        <div class="app--content">
+            <main class="app--main">
+                <nuxt/>
+            </main>
+            <app-footer/>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeader from "@global/TheHeader";
+import AppFooter from "@global/TheFooter";
 
-export default { components: { "app-header": AppHeader } };
+export default {
+    components: {
+        "app-header": AppHeader,
+        "app-footer": AppFooter
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-.app-wrapper {
+.app {
     background-color: $appBg1;
     height: 100%;
+
+    &--wrapper {
+        height: 100%;
+    }
+
+    &--menu {
+        width: 100%;
+        max-width: 200px;
+        background: {
+            color: $appBg2;
+        };
+    }
+
+    &--content {
+        width: 100%;
+    }
+
+    &--app-main {
+        padding: 20px;
+        overflow-x: scroll;
+    }
 }
 </style>
 
