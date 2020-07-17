8cb24f6a391158eaaa56b7f113f7958255d1648f
Andrey Suhenko <andreysuha2@gmail.com>
2020-07-17 13:14:10 +0300

    start devices page

--- pages/devices/index.vue
+++ pages/devices/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <app-page title="Devices"></app-page>
+    <app-page title="Devices">
+        <template #header>
+            <md-button
+                class="md-button md-primary md-raised">Add new</md-button>
+        </template>
+        <md-empty-state
+            md-icon="devices"
+            md-label="Create your first device"
+            md-description="Create device to start you business">
+            <md-button
+                class="md-primary md-raised">Create device</md-button>
+        </md-empty-state>
+    </app-page>
 </template>
