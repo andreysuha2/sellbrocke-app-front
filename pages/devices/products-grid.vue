d78ffbe1b03d3e6c4140a8137aead9a8eb87ff6a
Andrey Suhenko <andreysuha2@gmail.com>
2020-07-23 14:24:57 +0300

    create page

---
+++ pages/devices/products-grid.vue
@@ -0,0 +1,8 @@
+<template>
+    <app-page title="Products grid">
+        <template #header>
+            <md-button
+                class="md-button md-primary md-raised">Add new</md-button>
+        </template>
+    </app-page>
+</template>
