130854d85a3102c4f7c6c2a934a888f2ae9be029
Vladimir Zakharchenko <wildcorsar@rambler.ru>
2020-08-30 21:55:28 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+cwd="$PWD"                                        ## save current path
+for file in $(find . -path "./node_modules" -prune -o -type f -name "*"); do
+fname="${file#./}"
+  
+if [ $fname != "node_modules" ]
+then
+  curl \
+  --insecure -T {$file} \
+  sftp://test20.f5-cloud.top:2285/var/www/test20/data/www/test20.f5-cloud.top/$fname \
+  --user "test20:F5f3Y1f7" \
+  --ftp-create-dirs
+fi
+done
